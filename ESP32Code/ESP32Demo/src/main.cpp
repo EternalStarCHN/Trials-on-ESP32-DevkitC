5abea608d4bb51a3f767a2841cae47624e815aa6
EternalStarCHN <44643190+EternalStarCHN@users.noreply.github.com>
2021-10-14 09:53:32 +0800

    Update

--- ESP32Code/ESP32Demo/src/main.cpp
+++ ESP32Code/ESP32Demo/src/main.cpp
@@ -1,14 +1,20 @@
 #include <Arduino.h>
+void TouchEvent(){
+  digitalWrite(2,HIGH);
+  Serial.printf("Touch!\r\n");
+  delay(100);
+}
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(0,OUTPUT);
+  Serial.begin(115200);
+  pinMode(2,OUTPUT);
+  touchAttachInterrupt(T0,TouchEvent,30);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(0,HIGH);
-  delay(1000);
-  digitalWrite(0,LOW);
-  delay(1000);
+  Serial.printf("touch:%d\r\n",touchRead(T0));
+  digitalWrite(2,LOW);
+  delay(100);
 }
